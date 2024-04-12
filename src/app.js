b084f1c16054737f8de8c24f2c12efb0678f8105
Emoncr <biplobemon 75945@gmail.com>
2024-04-12 11:38:49 +0600

    setup - setup backend files

---
+++ src/app.js
@@ -0,0 +1,33 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+// Using CORS
+if (process.env.NODE_ENV === "production") {
+  app.use(
+    cors({ origin: "https://kena-kata-client.vercel.app/", credentials: true })
+  );
+} else {
+  app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+}
+
+// Using Express Middlewares
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+
+// Importing Routes
+import productRoute from "./routes/product.route.js";
+
+// Routes
+app.use("/api", productRoute);
+
+
+
+
+
+
+
+
+
+export default app;
